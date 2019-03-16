d623bf79c44962a5fc2ecc4d618944944a3c04e1
Your Name <you@example.com>
2019-03-16 15:43:13 -0700

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,21 @@
+
+participant ={'country': 'usa','language': 'Python'}
+print(participant['country'])
+participant['country'] = 'canada'
+print(participant['country'])
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
+
+def hello(name):
+	print('Hi ' + name)
+
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+	
+	hello(name)
+	print('name')
+    
